11a25b4170281c199c2930a04be65f7d8eb834b7
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-02-25 03:38:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function wakeDog(dogName, dogBreed){
-  let string = `Wake ${dogName} to ${dogBreed}`
+  let string = `Wake ${dogName} the ${dogBreed}`
   console.log(string)
 }
+
+function leashDog(dogName, dogBreed){
+  let string = `Leash ${dogName} to ${dogBreed}`
+  console.log(string)
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
