51ec6af679215e4e5536d6c290ce03fac2628cd9
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-02-25 03:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 function wakeDog(dogName, dogBreed){
   let string = `Wake ${dogName} the ${dogBreed}`
   console.log(string)
+   return string
 }
 
 function leashDog(dogName, dogBreed){
   let string = `Leash ${dogName} the ${dogBreed}`
   console.log(string)
+   return string
 }
 
 function unleashDog(dogName, dogBreed){
   let string = `Unleash ${dogName} the ${dogBreed}`
   console.log(string)
+  return string
 }
 
 function walkToPark(dogName, dogBreed){
   let string = `Walk ${dogName} the ${dogBreed} to the park`
   console.log(string)
+   return string
 }
 
 function throwFrisbee(dogName, dogBreed){
   let string = `Throw the frisbee with ${dogName} the ${dogBreed}`
   console.log(string)
+   return string
 }
 
 function walkHome(dogName, dogBreed){
   let string = `Walk ${dogName} the ${dogBreed} home`
   console.log(string)
+   return string
 }
 
 function exerciseDog(dogName, dogBreed){
